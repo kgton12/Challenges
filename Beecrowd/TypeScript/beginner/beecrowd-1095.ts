7f488e98e94bb71ea58ce5bdc24e9b7b1def03e6
Wellington Oliveira <56723945+kgton12@users.noreply.github.com>
2024-12-29 09:25:20 -0300

    Update beecrowd-1095.ts

--- Beecrowd/TypeScript/beginner/beecrowd-1095.ts
+++ Beecrowd/TypeScript/beginner/beecrowd-1095.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 import { EOL } from "node:os";
 import { stdin } from "node:process";
 
-const input = readFileSync(stdin.fd, { encoding: "utf8" }).split(EOL).map(Number);
+// const input = readFileSync(stdin.fd, { encoding: "utf8" }).split(EOL).map(Number);
 // const input = ['3',
 // 	'6.5 4.3 6.2',
 // 	'5.1 4.2 8.1',
