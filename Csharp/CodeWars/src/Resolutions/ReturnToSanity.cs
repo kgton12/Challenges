866a341b28f49866194693e2caf8e2faa1fcade1
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2025-07-25 19:21:49 -0300

    Adicionar classe ReturnToSanity com método Mystery
    Foi adicionado um novo namespace `CodeWars.src.Resolutions` e uma nova classe pública chamada `ReturnToSanity`. Dentro dessa classe, foi implementado um método estático `Mystery` que retorna um `Dictionary<string, string>` contendo uma única entrada, onde a chave é "sanity" e o valor é "hello".

---
+++ Csharp/CodeWars/src/Resolutions/ReturnToSanity.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeWars.src.Resolutions;
+
+public class ReturnToSanity
+{
+    public static Dictionary<string, string> Mystery() =>
+        new()
+        {
+            { "sanity", "hello" }
+        };
+}
