ec65a71b7535ad6c41f94e706b336d29e73b5a89
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2025-07-29 21:51:05 -0300

    Refatoração de métodos e adição de novas classes
    Removida a chamada ao método `MissingNo` em `Program.cs`, substituída pela chamada ao método `Solve` da classe `MaxminArrays`.
    
    Adicionada a classe `CreditCardIssuerChecking` com o método `GetIsSuer` para verificar emissores de cartões de crédito.
    
    Adicionada a classe `MaxminArrays` com o método `Solve`, que ordena uma lista de inteiros e retorna uma nova lista alternando entre os maiores e menores valores.

---
+++ Csharp/CodeWars/src/Resolutions/CreditCardIssuerChecking.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWars.src.Resolutions;
+
+public class CreditCardIssuerChecking
+{
+    public static string GetIsSuer(long number)
+    {
+        string str = number.ToString();
+
+        if ((str[..2] == "34" || str[..2] == "37") && str.Length == 15)
+            return "AMEX";
+        else if (str[..4] == "6011" && str.Length == 16)
+            return "Discover";
+        else if (new string[] { "51", "52", "53", "54", "55" }.Contains(str[..2]) && str.Length == 16)
+            return "Mastercard";
+        else if (str[..1] == "4" && (str.Length == 13 || str.Length == 16))
+            return "VISA";
+        else return "Unknown";
+    }
+}
