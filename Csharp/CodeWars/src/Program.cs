6927a70eeb7a1a5bf1ca605fdd431d2606fae826
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2025-08-15 19:54:05 -0300

    Atualiza namespaces e adiciona novas classes
    Removido o namespace `CodeWars.src` de `Program.cs` e adicionado um método que chama `ConsecutiveLetters.Solve`.
    
    Alterado o namespace de `HelpSuzukiRakeHisGarden.cs` para `CodeWars.src.Resolutions`.
    
    Adicionada a classe `HellsKitchen` em `HellsKitchen.cs`, que implementa métodos para transformar strings com regras específicas.
    
    Implementada a classe `WhosOnlineClass` em `WhosOnline.cs`, que define um enum `UserStatus` e um método estático `WhosOnline` para agrupar usuários por status.

--- Csharp/CodeWars/src/Program.cs
+++ Csharp/CodeWars/src/Program.cs
@@ -1,6 +1,6 @@
-﻿using CodeWars.src;
+﻿
 
-Console.WriteLine(ConsecutiveLetters.Solve("dabc"));
+
 
 
 //foreach (var item in TailSwapClass.TailSwap(["abc:123", "cde:456"]))
---
+++ Csharp/CodeWars/src/Resolutions/HellsKitchen.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWars.src.Resolutions;
+
+public class HellsKitchen
+{
+    public static string Gordon(string a) =>
+        string.Join(" ", a.Split(' ').Select(GordonString));
+
+    private static string GordonString(string word) =>
+        string.Concat(
+            word
+            .Select(x => "eiou".Contains(char.ToLower(x)) ? '*' : char.ToLower(x) == 'a' ? '@' : char.ToUpper(x))
+        ) + "!!!!";
+}
