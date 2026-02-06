d0438dc34f6fc95c0963cb996de0d58c56e7de50
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-02-06 16:34:07 -0300

    New katas/tests solveds

---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/CompleteThePattern6OddLadderTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars.Resolutions;
+
+namespace CodeWarsTest.Completed;
+
+public class CompleteThePattern6OddLadderTest
+{
+    [Test]
+    public void Test_3()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(CompleteThePattern6OddLadder.OddLadder(4), Is.EqualTo("1\n333"), "Nope!");
+            Assert.That(CompleteThePattern6OddLadder.OddLadder(7), Is.EqualTo("1\n333\n55555\n7777777"), "Nope!");
+            Assert.That(CompleteThePattern6OddLadder.OddLadder(10), Is.EqualTo("1\n333\n55555\n7777777\n999999999"), "Nope!");
+        }
+    }
+}
