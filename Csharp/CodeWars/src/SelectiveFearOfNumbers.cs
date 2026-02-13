62b4575331f5b6b217773fb1c17bc869a5943c79
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-02-12 21:59:21 -0300

    new kata solved

---
+++ Csharp/CodeWars/src/SelectiveFearOfNumbers.cs
@@ -0,0 +1,34 @@
+﻿namespace CodeWars;
+
+public class SelectiveFearOfNumbers
+{
+    private enum DaysWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    public static bool AmIAfraid(string day, int num)
+    {
+        if (day == DaysWeek.Monday.ToString() && num == 12)
+            return true;
+        else if (day == DaysWeek.Tuesday.ToString() && num > 95)
+            return true;
+        else if (day == DaysWeek.Wednesday.ToString() && num == 34)
+            return true;
+        else if (day == DaysWeek.Thursday.ToString() && num == 0)
+            return true;
+        else if (day == DaysWeek.Friday.ToString() && num % 2 == 0)
+            return true;
+        else if (day == DaysWeek.Saturday.ToString() && num == 56)
+            return true;
+        else if (day == DaysWeek.Sunday.ToString() && (num == 666 || num == -666))
+            return true;
+        else
+            return false;
+    }
+}
