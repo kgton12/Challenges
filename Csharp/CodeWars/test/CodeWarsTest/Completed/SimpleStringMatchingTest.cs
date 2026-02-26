8bd58a686ce5d4472e7608803b3f8219e5d2de71
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-02-26 20:54:44 -0300

    new katas solveds

---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/SimpleStringMatchingTest.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars.Resolutions;
+
+namespace CodeWarsTest.Completed;
+
+public class SimpleStringMatchingTest
+{
+    [Test]
+    public void SampleTest()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(SimpleStringMatching.Solve("code*s", "codewars"), Is.True);
+            Assert.That(SimpleStringMatching.Solve("codewar*s", "codewars"), Is.True);
+            Assert.That(SimpleStringMatching.Solve("*c", "c"), Is.True);
+            Assert.That(SimpleStringMatching.Solve("*", "asterisk"), Is.True);
+            Assert.That(SimpleStringMatching.Solve("get*", "getreal"), Is.True);
+            Assert.That(SimpleStringMatching.Solve("*less", "restless"), Is.True);
+            Assert.That(SimpleStringMatching.Solve("d*oll", "dual"), Is.False);
+            Assert.That(SimpleStringMatching.Solve("*osd", "asterisk"), Is.False);
+            Assert.That(SimpleStringMatching.Solve("*ea", "bean"), Is.False);
+            Assert.That(SimpleStringMatching.Solve("a", "b"), Is.False);
+        }
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/ZebulansNightmareClassTest.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars.Resolutions;
+
+namespace CodeWarsTest.Completed;
+
+public class ZebulansNightmareClassTest
+{
+    [Test]
+    public void BasicTests()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(ZebulansNightmareClass.ZebulansNightmare("camel_case"), Is.EqualTo("camelCase"));
+            Assert.That(ZebulansNightmareClass.ZebulansNightmare("mark_as_issue"), Is.EqualTo("markAsIssue"));
+            Assert.That(ZebulansNightmareClass.ZebulansNightmare("copy_paste_pep8"), Is.EqualTo("copyPastePep8"));
+            Assert.That(ZebulansNightmareClass.ZebulansNightmare("goto_next_kata"), Is.EqualTo("gotoNextKata"));
+            Assert.That(ZebulansNightmareClass.ZebulansNightmare("repeat"), Is.EqualTo("repeat"));
+            Assert.That(ZebulansNightmareClass.ZebulansNightmare("trolling_is_fun"), Is.EqualTo("trollingIsFun"));
+            Assert.That(ZebulansNightmareClass.ZebulansNightmare("why"), Is.EqualTo("why"));
+            Assert.That(ZebulansNightmareClass.ZebulansNightmare("123_abc_def"), Is.EqualTo("123AbcDef"));
+        }
+    }
+}
