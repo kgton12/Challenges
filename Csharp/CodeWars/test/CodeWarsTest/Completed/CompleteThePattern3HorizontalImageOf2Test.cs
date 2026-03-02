03ce2d164808243d502fbf9bd0dc0a0be0b2af56
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-03-02 13:41:40 -0300

    new katas solveds

---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/CompleteThePattern3HorizontalImageOf2Test.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars.Resolutions;
+
+namespace CodeWarsTest.Completed;
+
+public class CompleteThePattern3HorizontalImageOf2Test
+{
+    [Test]
+    public void BasicTests()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(CompleteThePattern3HorizontalImageOf2.Pattern(-3), Is.EqualTo(""));
+            Assert.That(CompleteThePattern3HorizontalImageOf2.Pattern(0), Is.EqualTo(""));
+            Assert.That(CompleteThePattern3HorizontalImageOf2.Pattern(1), Is.EqualTo("1"));
+            Assert.That(CompleteThePattern3HorizontalImageOf2.Pattern(2), Is.EqualTo("2\n21"));
+            Assert.That(CompleteThePattern3HorizontalImageOf2.Pattern(5), Is.EqualTo("5\n54\n543\n5432\n54321"));
+        }
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/TurnAnyWordIntoABeefTacoTest.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars.Resolutions;
+
+namespace CodeWarsTest.Completed;
+
+public class TurnAnyWordIntoABeefTacoTest
+{
+    [Test]
+    public void BasicTest()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(TurnAnyWordIntoABeefTaco.Tacofy(""), Is.EqualTo(["shell", "shell"]));
+            Assert.That(TurnAnyWordIntoABeefTaco.Tacofy("a"), Is.EqualTo(["shell", "beef", "shell"]));
+            Assert.That(TurnAnyWordIntoABeefTaco.Tacofy("ggg"), Is.EqualTo(["shell", "guacamole", "guacamole", "guacamole", "shell"]));
+            Assert.That(TurnAnyWordIntoABeefTaco.Tacofy("ogl"), Is.EqualTo(["shell", "beef", "guacamole", "lettuce", "shell"]));
+            Assert.That(TurnAnyWordIntoABeefTaco.Tacofy("ydjkpwqrzto"), Is.EqualTo(["shell", "tomato", "beef", "shell"]));
+        }
+    }
+}
