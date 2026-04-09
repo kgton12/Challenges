25d55ed2bd5c0cb7b1c362e4692b059fbbfbbcc6
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-04-09 16:38:17 -0300

    Refatora HistogramV1 e adiciona teste básico
    - Move HistogramV1 para CodeWars.Completed e reimplementa lógica para melhor alinhamento e suporte a entradas nulas/vazias.
    - Atualiza CodeWars.csproj para incluir pasta "Incomplete".
    - Adiciona HistogramV1Test.cs com teste básico de validação do histograma.

---
+++ Csharp/CodeWars/src/Completed/HistogramV1.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+
+namespace CodeWars.Completed;
+
+public class HistogramV1
+{
+    public static string Histogram(int[] results)
+    {
+        results ??= [];
+
+        int n = results.Length;
+        int max = n > 0 ? results.Max() : 0;
+        var sb = new StringBuilder();
+
+        int baseWidth = n > 0 ? 2 * n - 1 : 0;
+
+        if (max > 0)
+        {
+            for (int r = 0; r <= max; r++)
+            {
+                var lineSb = new StringBuilder(new string(' ', baseWidth));
+
+                for (int i = 0; i < n; i++)
+                {
+                    int pos = i * 2;
+                    int c = results[i];
+                    int rNumber = max - c;
+
+                    if (c > 0 && r == rNumber)
+                    {
+                        string s = c.ToString();
+                        if (pos + s.Length > lineSb.Length)
+                            lineSb.Append(new string(' ', pos + s.Length - lineSb.Length));
+                        for (int k = 0; k < s.Length; k++) lineSb[pos + k] = s[k];
+                    }
+                    else if (r >= (max - c + 1) && r <= max)
+                    {
+                        if (pos < lineSb.Length) lineSb[pos] = '#';
+                    }
+                }
+
+                sb.Append(lineSb.ToString().TrimEnd());
+                sb.Append('\n');
+            }
+        }
+
+        if (baseWidth > 0)
+        {
+            sb.Append(new string('-', baseWidth));
+            sb.Append('\n');
+
+            var labelSb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                if (i > 0) labelSb.Append(' ');
+                labelSb.Append(i + 1);
+            }
+            sb.Append(labelSb);
+            sb.Append('\n');
+        }
+        else
+        {
+            sb.Append(new string('-', 0));
+            sb.Append('\n');
+            sb.Append('\n');
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/HistogramV1Test.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+public class HistogramV1Test
+{
+    [Test]
+    public void BasicTest()
+    {
+        string expected =
+        "    10\n" +
+        "    #\n" +
+        "    #\n" +
+        "7   #\n" +
+        "#   #\n" +
+        "#   #     5\n" +
+        "#   #     #\n" +
+        "# 3 #     #\n" +
+        "# # #     #\n" +
+        "# # # 1   #\n" +
+        "# # # #   #\n" +
+        "-----------\n" +
+        "1 2 3 4 5 6\n";
+        Assert.That(HistogramV1.Histogram([7, 3, 10, 1, 0, 5]), Is.EqualTo(expected));
+    }
+}
