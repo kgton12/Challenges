6cabb8f89c1141112f275350b889fc0388bf2ffb
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-05-14 21:30:08 -0300

    Adiciona SprayingTrees e testes unitários básicos
    Inclui a classe SprayingTrees com lógica para associar dias da semana a trabalhadores e calcular o custo de pulverização. Adiciona testes unitários usando NUnit para validar o método Task com diferentes entradas.

---
+++ Csharp/CodeWars/src/Completed/SprayingTrees.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeWars.Completed
+{
+    public class SprayingTrees
+    {
+        private static readonly Dictionary<string, string> workers =
+            new()
+            {
+                {"Monday", "James"},
+                {"Tuesday", "John"},
+                {"Wednesday", "Robert"},
+                {"Thursday", "Michael"},
+                {"Friday", "William"}
+            };
+
+        public static string Task(string w, int n, int c) =>
+            $"It is {w} today, {workers[w]}, you have to work, you must spray {n} trees and you need {n * c} dollars to buy liquid";
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/SprayingTreesTest.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+public class SprayingTreesTest
+{
+    private static readonly object[] Basic_Test_Cases =
+        [
+          new object[] {"Wednesday", 10, 2, "It is Wednesday today, Robert, you have to work, you must spray 10 trees and you need 20 dollars to buy liquid"},
+          new object[] {"Monday", 4, 3, "It is Monday today, James, you have to work, you must spray 4 trees and you need 12 dollars to buy liquid"},
+          new object[] {"Friday", 5, 4, "It is Friday today, William, you have to work, you must spray 5 trees and you need 20 dollars to buy liquid"},
+          new object[] {"Tuesday", 6, 1, "It is Tuesday today, John, you have to work, you must spray 6 trees and you need 6 dollars to buy liquid"},
+          new object[] {"Thursday", 5, 3, "It is Thursday today, Michael, you have to work, you must spray 5 trees and you need 15 dollars to buy liquid"},
+        ];
+
+    [Test, TestCaseSource(typeof(SprayingTreesTest), nameof(Basic_Test_Cases))]
+    public void Basic_Test(string w, int n, int c, string expected)
+    {
+        Assert.That(SprayingTrees.Task(w, n, c), Is.EqualTo(expected));
+    }
+}
