e4ed93bdb2f6b768e5e93a1254d3a48f8fc3849d
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-05-23 15:19:15 -0300

    Solveds new katas

---
+++ Csharp/CodeWars/src/ConvertIntegerToBinary.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWars;
+
+public class ConvertIntegerToBinary
+{
+    public static string ToBinary(int n) =>
+        Convert.ToString(n, 2);
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/ConvertIntegerToBinaryTest.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars;
+
+namespace CodeWarsTest;
+
+public class ConvertIntegerToBinaryTest
+{
+    [Test]
+    public void BasicTests()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(ConvertIntegerToBinary.ToBinary(0), Is.EqualTo("0"));
+            Assert.That(ConvertIntegerToBinary.ToBinary(2), Is.EqualTo("10"));
+            Assert.That(ConvertIntegerToBinary.ToBinary(3), Is.EqualTo("11"));
+            Assert.That(ConvertIntegerToBinary.ToBinary(4), Is.EqualTo("100"));
+            Assert.That(ConvertIntegerToBinary.ToBinary(5), Is.EqualTo("101"));
+        }
+    }
+
+    [Test]
+    public static void Negative_Numbers_Tests()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(ConvertIntegerToBinary.ToBinary(-3), Is.EqualTo("11111111111111111111111111111101"));
+            Assert.That(ConvertIntegerToBinary.ToBinary(-2066410621), Is.EqualTo("10000100110101010001001110000011"));
+        }
+    }
+}
