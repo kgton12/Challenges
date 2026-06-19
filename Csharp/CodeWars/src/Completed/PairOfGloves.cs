17731344d29f64a60e0323692e8b41d083b79115
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-06-19 20:39:58 -0300

    Adiciona PairOfGloves e testes unitários associados
    Implementa a classe PairOfGloves com método NumberOfPairs para contar pares de luvas por cor. Inclui testes unitários cobrindo múltiplos cenários de entrada.

---
+++ Csharp/CodeWars/src/Completed/PairOfGloves.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeWars.Completed;
+
+public class PairOfGloves
+{
+    public static int NumberOfPairs(string[] gloves) =>
+        gloves.GroupBy(x => x)
+              .Sum(x => x.Count() / 2);
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/PairOfGlovesTest.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+public class PairOfGlovesTest
+{
+    [Test]
+    public void SampleTests()
+    {
+        Assertion(0, ["Green", "Blue", "Purple", "Gray"]);
+        Assertion(0, []);
+        Assertion(0, ["Purple"]);
+
+        Assertion(1, ["Blue", "Purple", "Blue", "Gray", "Lime", "Black"]);
+        Assertion(1, ["Blue", "Aqua", "Blue", "Teal", "Blue", "Black"]);
+
+        Assertion(2, ["Blue", "Aqua", "Blue", "Brown", "Blue", "Orange", "Aqua"]);
+    }
+
+    private static void Assertion(int expected, string[] input) =>
+      Assert.That(
+        PairOfGloves.NumberOfPairs(input),
+        Is.EqualTo(expected),
+        $"Input: [{string.Join(", ", input)}]"
+      );
+}
