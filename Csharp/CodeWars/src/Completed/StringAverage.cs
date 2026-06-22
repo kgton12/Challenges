db014043a1968083b1f181f4593ac702735e4746
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-06-22 15:28:17 -0300

    Adiciona StringAverage e testes unitários
    Implementa a classe StringAverage para calcular a média de números escritos por extenso em inglês, retornando o resultado também por extenso ou "n/a" para casos inválidos. Inclui testes unitários cobrindo entradas válidas e inválidas.

---
+++ Csharp/CodeWars/src/Completed/StringAverage.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeWars.Completed;
+
+public class StringAverage
+{
+    private static readonly Dictionary<string, int> valuePairs =
+        new()
+        {
+            ["zero"] = 0,
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+            ["four"] = 4,
+            ["five"] = 5,
+            ["six"] = 6,
+            ["seven"] = 7,
+            ["eight"] = 8,
+            ["nine"] = 9
+        };
+    public static string AverageString(string str)
+    {
+        string[] splitedStr = str.Split(' ');
+
+        if (string.IsNullOrEmpty(str.Trim()) || !splitedStr.All(x => valuePairs.TryGetValue(x, out int _)))
+            return "n/a";
+
+        int avg = (int)splitedStr.Average(x => valuePairs.TryGetValue(x, out int value) ? value : 0);
+
+        return valuePairs.FirstOrDefault(x => x.Value == avg).Key;
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/StringAverageTest.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+public class StringAverageTest
+{
+    [Test]
+    public void SampleTests()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(StringAverage.AverageString("zero nine five two"), Is.EqualTo("four"));
+            Assert.That(StringAverage.AverageString("four six two three"), Is.EqualTo("three"));
+            Assert.That(StringAverage.AverageString("one two three four five"), Is.EqualTo("three"));
+            Assert.That(StringAverage.AverageString("five four"), Is.EqualTo("four"));
+            Assert.That(StringAverage.AverageString("zero zero zero zero zero"), Is.EqualTo("zero"));
+            Assert.That(StringAverage.AverageString("one one eight one"), Is.EqualTo("two"));
+            Assert.That(StringAverage.AverageString(""), Is.EqualTo("n/a"));
+            Assert.That(StringAverage.AverageString("nine, wedwmw, three, zero"), Is.EqualTo("n/a"));
+        }
+    }
+}
