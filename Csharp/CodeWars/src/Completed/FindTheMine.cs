ce8b32784eb333a6398e70b61423c2c19beb4661
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-06-27 08:41:17 -0300

    Adiciona FindTheMine e testes unitários com NUnit
    Inclui a classe FindTheMine com o método MineLocation para localizar a mina em uma matriz. Adiciona testes unitários parametrizados usando NUnit para validar o funcionamento do método.

---
+++ Csharp/CodeWars/src/Completed/FindTheMine.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeWars.Completed;
+
+public class FindTheMine
+{
+    public static Tuple<int, int> MineLocation(int[,] field)
+    {
+        for (int row = 0; row < field.GetLength(0); row++)
+        {
+            for (int col = 0; col < field.GetLength(1); col++)
+            {
+                if (field[row, col] == 1)
+                    return Tuple.Create(row, col);
+            }
+        }
+        return Tuple.Create(-1, -1);
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/FindTheMineTest.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+public class FindTheMineTest
+{
+    private static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(new int[,] { { 1, 0 }, { 0, 0 } }).Returns(new Tuple<int, int>(0, 0));
+            yield return new TestCaseData(new int[,] { { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } }).Returns(new Tuple<int, int>(0, 0));
+            yield return new TestCaseData(new int[,] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 0 } }).Returns(new Tuple<int, int>(2, 2));
+        }
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public Tuple<int, int> Test(int[,] field) => FindTheMine.MineLocation(field);
+}
