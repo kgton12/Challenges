f306d1a914267951352894eb22f4a7d49ba41c80
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-07-07 18:39:10 -0300

    Adiciona NumericalsOfAString e testes unitários
    Implementa a classe NumericalsOfAString com o método estático Numericals, que converte cada caractere de uma string em sua contagem sequencial de ocorrências. Inclui testes unitários usando NUnit para validar o comportamento do método.

---
+++ Csharp/CodeWars/src/Completed/NumericalsOfAString.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWars.Completed;
+
+public class NumericalsOfAString
+{
+    public static string Numericals(string s)
+    {
+        Dictionary<char, int> score = [];
+
+        return string.Concat(
+            s.Select(actualChar =>
+            {
+                if (score.TryGetValue(actualChar, out int value))
+                    score[actualChar] += 1;
+                else
+                    score.Add(actualChar, 1);
+
+                return score[actualChar];
+            })
+        );
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/NumericalsOfAStringTest.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+public class NumericalsOfAStringTest
+{
+    [Test, Order(1)]
+    public void BasicTest1()
+    {
+        Assert.That(NumericalsOfAString.Numericals("Hello, World!"), Is.EqualTo("1112111121311"));
+    }
+
+    [Test, Order(2)]
+    public void BasicTest2()
+    {
+        Assert.That(NumericalsOfAString.Numericals("Hello, World! It's me, JomoPipi!"), Is.EqualTo("11121111213112111131224132411122"));
+    }
+
+    [Test, Order(3)]
+    public void BasicTest3()
+    {
+        Assert.That(NumericalsOfAString.Numericals("hello hello"), Is.EqualTo("11121122342"));
+    }
+}
