75c1828a9ddbe854c8fff887029229ec22799605
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-07-08 13:18:41 -0300

    Implementa NumberZooPatrol e testes unitários com NUnit
    Adiciona a classe NumberZooPatrol com o método FindNumber para identificar o menor número inteiro positivo ausente em um array. Inclui testes unitários abrangentes usando NUnit para validar diferentes cenários, incluindo arrays desordenados e casos especiais.

---
+++ Csharp/CodeWars/src/Completed/NumberZooPatrol.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWars.Completed;
+
+public class NumberZooPatrol
+{
+    public static int FindNumber(int[] array)
+    {
+        HashSet<int> sortedNumbers = [.. array.OrderBy(x => x)];
+
+        if (!sortedNumbers.TryGetValue(1, out _))
+            return 1;
+
+        foreach (var item in sortedNumbers)
+        {
+            if (!sortedNumbers.Contains(item + 1))
+                return item + 1;
+        }
+
+        return -1;
+    }
+}
