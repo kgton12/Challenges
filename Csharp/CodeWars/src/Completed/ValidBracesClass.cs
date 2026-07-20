1c75135bc18cc1f48c79d7483646df76c97af3f1
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-07-20 15:38:40 -0300

    Adds ValidBracesClass and NUnit unit tests
    Includes ValidBracesClass.cs, a class with a method to validate the balancing of parentheses, brackets, and braces using a stack and a dictionary. Adds ValidBracesClassTest.cs with five unit tests covering balanced and unbalanced cases, using the NUnit framework.

---
+++ Csharp/CodeWars/src/Completed/ValidBracesClass.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeWars.Completed;
+
+public class ValidBracesClass
+{
+    public static bool ValidBraces(string s)
+    {
+        Stack<char> stack = new();
+        Dictionary<char, char> pairs = new()
+        {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        foreach (char ch in s)
+        {
+            if (ch == '(' || ch == '[' || ch == '{')
+            {
+                stack.Push(ch);
+            }
+            else
+            {
+                if (stack.Count == 0 || stack.Pop() != pairs[ch])
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/ValidBracesClassTest.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+public class ValidBracesClassTest
+{
+    [Test, Order(1)]
+    public void Test1()
+    {
+        Assert.That(ValidBracesClass.ValidBraces("(){}[]"), Is.True);
+    }
+
+    [Test, Order(2)]
+    public void Test2()
+    {
+        Assert.That(ValidBracesClass.ValidBraces("([{}])"), Is.True);
+    }
+    [Test, Order(3)]
+    public void Test3()
+    {
+        Assert.That(ValidBracesClass.ValidBraces("(}"), Is.False);
+    }
+    [Test, Order(4)]
+    public void Test4()
+    {
+        Assert.That(ValidBracesClass.ValidBraces("[(])"), Is.False);
+    }
+    [Test, Order(5)]
+    public void Test5()
+    {
+        Assert.That(ValidBracesClass.ValidBraces("[({})](]"), Is.False);
+    }
+}
