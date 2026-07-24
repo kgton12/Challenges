53ccc945e5eb91f2cacbc9dbce6d0bc45aa29799
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-07-23 21:59:31 -0300

    Adds Roman numeral decoder and tests
    The files `RomanNumeralsDecoder.cs` (implementing the conversion from Roman numerals to integers) and `RomanNumeralsDecoderTest.cs` (containing NUnit unit tests to validate various conversion cases) were created.

---
+++ Csharp/CodeWars/src/Completed/RomanNumeralsDecoder.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeWars.Completed;
+
+public class RomanNumeralsDecoder
+{
+    public static readonly Dictionary<char, int> romanNumerals =
+    new()
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 },
+    };
+
+    public static int Solution(string roman) =>
+        roman.Select((Value, Index) => new { Value, Index }).Sum(x =>
+        {
+            if (x.Index + 1 == roman.Length)
+                return romanNumerals[roman[x.Index]];
+            else
+            {
+                if (romanNumerals[roman[x.Index]] < romanNumerals[roman[x.Index + 1]])
+                    return -romanNumerals[roman[x.Index]];
+                else
+                    return romanNumerals[roman[x.Index]];
+            }
+        });
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/RomanNumeralsDecoderTest.cs
@@ -0,0 +1,48 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+public class RomanNumeralsDecoderTest
+{
+    [TestCase(1, "I")]
+    public void Test1(int expected, string roman)
+    {
+        Assert.That(RomanNumeralsDecoder.Solution(roman), Is.EqualTo(expected));
+    }
+
+    [TestCase(2000, "MM")]
+    public void Test2(int expected, string roman)
+    {
+        Assert.That(RomanNumeralsDecoder.Solution(roman), Is.EqualTo(expected));
+    }
+
+    [TestCase(1666, "MDCLXVI")]
+    public void Test3(int expected, string roman)
+    {
+        Assert.That(RomanNumeralsDecoder.Solution(roman), Is.EqualTo(expected));
+    }
+
+    [TestCase(1000, "M")]
+    public void Test4(int expected, string roman)
+    {
+        Assert.That(RomanNumeralsDecoder.Solution(roman), Is.EqualTo(expected));
+    }
+
+    [TestCase(400, "CD")]
+    public void Test5(int expected, string roman)
+    {
+        Assert.That(RomanNumeralsDecoder.Solution(roman), Is.EqualTo(expected));
+    }
+
+    [TestCase(90, "XC")]
+    public void Test6(int expected, string roman)
+    {
+        Assert.That(RomanNumeralsDecoder.Solution(roman), Is.EqualTo(expected));
+    }
+
+    [TestCase(40, "XL")]
+    public void Test7(int expected, string roman)
+    {
+        Assert.That(RomanNumeralsDecoder.Solution(roman), Is.EqualTo(expected));
+    }
+}
