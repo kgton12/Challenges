b52637b6b1513e8d7b3559d3da50b9f5a660127c
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-08-29 08:40:16 -0300

    Added StonePickaxeCrafting and NUnit unit tests
    Created `StonePickaxeCrafting.cs` with the static `StonePick` method to calculate the maximum number of stone pickaxes that can be crafted from materials ("Cobblestone", "Sticks", and "Wood"), taking conversion rules into account.
    Includes `StonePickTest.cs` with an NUnit unit test suite covering various scenarios, invalid cases, and random example generation. Helper functions were implemented to handle material arrays.

---
+++ Csharp/CodeWars/src/Completed/StonePickaxeCrafting.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWars.Completed;
+
+public class StonePickaxeCrafting
+{
+    public static int StonePick(string[] arr)
+    {
+        int sumCobblestones = arr.Count(x => x == "Cobblestone");
+        int sumSticks = arr.Count(x => x == "Sticks");
+        sumSticks += arr.Count(x => x == "Wood") * 4;
+
+        return Math.Min(sumCobblestones / 3, sumSticks / 2);
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/StonePickTest.cs
@@ -0,0 +1,114 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed
+{
+    internal class StonePickTest
+    {
+        private static readonly Random rnd = new();
+
+        [TestCase("SSSSCCCCCC", 2)]
+        [TestCase("SSC", 0)]
+        [TestCase("SSSSwwwDDDDDD", 0)]
+        [TestCase("WWCCCCCCCCCCCC", 4)]
+        public void ExampleTestCase(string testString, int expected)
+        {
+            var arr = StringToMinecraftArrayConverter(testString);
+            var actual = StonePickaxeCrafting.StonePick(arr);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("SSSSCCC", 1)]
+        [TestCase("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSCCCCCCCCCCCCCCCCCCCCCCCCC", 8)]
+        [TestCase("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS" +
+            "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC", 21)]
+        public void PicksGreaterThanOrEqualTo1(string testString, int expected)
+        {
+            var arr = StringToMinecraftArrayConverter(testString);
+            var actual = StonePickaxeCrafting.StonePick(arr);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("SWDdsCSCC", 1)]
+        [TestCase("wDdSCC", 0)]
+        [TestCase("wwwwwwwwwwSSSSSSSSSSSsssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC" +
+            "CCCCCCCCCCCdddddddd", 5)]
+        public void PicksWithNonEligibleMaterials(string testString, int expected)
+        {
+            var arr = StringToMinecraftArrayConverter(testString);
+            var actual = StonePickaxeCrafting.StonePick(arr);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("wDdS", 0)]
+        [TestCase("WDCS", 0)]
+        [TestCase("DDDDDDDDDDDDDDDCCCCCCCCCCCCCCCCCCCCSwwwwwwwwwwwwwwwddddddddddd", 0)]
+        public void NoPicks(string testString, int expected)
+        {
+            var arr = StringToMinecraftArrayConverter(testString);
+            var actual = StonePickaxeCrafting.StonePick(arr);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW" +
+            "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC" +
+            "CCCCCCCCCCCCCCCCCCCCC", 30)]
+        [TestCase("WWWWWWWWWWWWCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC" +
+            "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC", 24)]
+        [TestCase("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWCCCCCCCCCCCCCCCCCCCC" +
+            "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCC", 16)]
+        public void PicksWithWoodAndSticksOnly(string testString, int expected)
+        {
+            var arr = StringToMinecraftArrayConverter(testString);
+            var actual = StonePickaxeCrafting.StonePick(arr);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void NoMaterials()
+        {
+            var arr = StringToMinecraftArrayConverter("");
+            var actual = StonePickaxeCrafting.StonePick(arr);
+            Assert.That(actual, Is.Zero);
+        }
+
+        [Test]
+        public void RandomTests()
+        {
+            var strings = GenerateRandomExamples();
+            foreach (var str in strings)
+            {
+                var arr = StringToMinecraftArrayConverter(str);
+                var actual = StonePickaxeCrafting.StonePick(arr);
+                var expected = Math.Min(arr.Sum(x => x == "Cobblestone" ? 1 : 0) / 3,
+                arr.Sum(x => x == "Wood" ? 4 : x == "Sticks" ? 1 : 0) / 2);
+                Assert.That(actual, Is.EqualTo(expected), "Arr => " + String.Join(", ", arr.Select(x => x)));
+            }
+        }
+
+        private static string[] StringToMinecraftArrayConverter(string s)
+        {
+            Dictionary<char, string> map = new()
+            { {'S', "Sticks"} , {'s', "Stone"}, {'W', "Wood" }, {'w', "Wool" },
+            {'C', "Cobblestone" }, {'D', "Dirt" }, {'d', "Diamond" } };
+            return [.. s.Select(x => map[x])];
+        }
+
+        private static string[] GenerateRandomExamples()
+        {
+            string[] arr = new string[100];
+            string baseChars = "SsWwCCCCCCDd";
+            for (int i = 0; i < 100; i++)
+            {
+                string inject = "";
+                int l = rnd.Next(0, 200);
+                for (int j = 0; j < l; j++)
+                {
+                    char letter = baseChars[rnd.Next(0, 12)];
+                    inject += letter;
+                }
+                arr[i] = inject;
+            }
+            return arr;
+        }
+    }
+}
