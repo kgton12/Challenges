7aea3ff7b349f7711ef11137c885b83c2bc693cc
wellington Malheiro de Oliveira <wellington.malheiro@gmail.com>
2026-09-12 14:28:39 -0300

    Adds AssembleString and unit tests
    Includes AssembleString.cs with a static `Assemble` method to reconstruct strings from copies, replacing '*' with known characters or '#' if all are '*'. Adds AssembleStringTest.cs with unit tests covering examples, edge cases, and random tests.

---
+++ Csharp/CodeWars/src/Completed/AssembleString.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeWars.Completed;
+
+public class AssembleString
+{
+    public static string Assemble(string[] copies)
+    {
+        if (copies.Length == 0) return "";
+
+        int lineLength = copies[0].Length;
+        string result = string.Empty;
+
+        for (int i = 0; i < lineLength; i++)
+        {
+            char chr = '#';
+            for (int j = 0; j < copies.Length; j++)
+            {
+                string word = copies[j];
+
+                if (word[i] != '*')
+                    chr = word[i];
+            }
+
+            result += chr;
+        }
+
+        return result;
+    }
+}
---
+++ Csharp/CodeWars/test/CodeWarsTest/Completed/AssembleStringTest.cs
@@ -0,0 +1,68 @@
+﻿using CodeWars.Completed;
+
+namespace CodeWarsTest.Completed;
+
+[TestFixture]
+public class AssembleStringTest
+{
+    private static void DoTest(string[] input, string expected)
+    {
+        string message = String.Format("\ninput = {{\n{0}\n}}\n",
+            string.Join(",\n", input.Select(s => "    \"" + s + "\"")));
+        string actual = AssembleString.Assemble(input);
+        Assert.That(actual, Is.EqualTo(expected), message);
+    }
+
+    [Test, Order(1)]
+    public void SampleTests()
+    {
+        DoTest(["a*cde", "*bcde", "abc*e"], "abcde");
+        DoTest(["a*c**", "**cd*", "a*cd*"], "a#cd#");
+        DoTest(["*ashtag ** *", "h*sht*g *> *", "has*tag -* *"], "hashtag -> #");
+    }
+
+    [Test, Order(2)]
+    public void SpecialTests()
+    {
+        DoTest(["abcde", "abcde", "abcde"], "abcde");
+        DoTest(["*****", "*****", "*****"], "#####");
+        DoTest([], "");
+        DoTest(["", "", ""], "");
+    }
+
+    [Test, Order(3)]
+    public void RandomTests()
+    {
+        Random random = new();
+        for (int test = 0; test < 100; test++)
+        {
+            int rows = random.Next(1, 15);
+            int cols = random.Next(1, 15);
+
+            char[] clearText = new char[cols];
+            for (int i = 0; i < cols; i++)
+                clearText[i] = (char)random.Next('a', 'z' + 1);
+
+            char[] deciphered = new char[cols];
+            Array.Fill(deciphered, '#');
+
+            char[][] matrix = new char[rows][];
+            for (int row = 0; row < rows; row++)
+                matrix[row] = new char[cols];
+
+            int star_percent = random.Next(100 + 1);
+            for (int col = 0; col < cols; col++)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    char ch = (random.Next(0, 100) < star_percent) ? '*' : clearText[col];
+                    matrix[row][col] = ch;
+                    if (ch != '*')
+                        deciphered[col] = ch;
+                }
+            }
+            string[] strings = [.. matrix.Select(chars => new string(chars))];
+            DoTest(strings, new string(deciphered));
+        }
+    }
+}
